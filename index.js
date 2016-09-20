c42fc4ff4a0bff1a35a87ee9a578b2139606961b
Aleksandra <koneska.aleksandra@gmail.com>
2016-09-20 16:07:31 -0400

    Create Echo bot

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.listen((process.env.PORT || 3000));
 
-// Server frontpage
-app.get('/', function (req, res) {
-    res.send('This is TestBot Server');
-});
+// // Server frontpage
+// app.get('/', function (req, res) {
+//     res.send('This is TestBot Server');
+// });
 
 // Facebook Webhook
 app.get('/webhook', function (req, res) {
